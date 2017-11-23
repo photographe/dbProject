87556b71b87da3f76d524288c9fac51a731e8a81
adudi1 <adudi1@umbc.edu>
2017-11-22 23:30:04 -0500

    db connection

--- DB_PresentationLayer/DataAccessLayer/DALLoginHandler.cs
+++ DB_PresentationLayer/DataAccessLayer/DALLoginHandler.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DB_PresentationLayer.DataAccessLayer
 {
@@ -9,7 +11,15 @@ namespace DB_PresentationLayer.DataAccessLayer
     {
         public bool IsLoginValid(string userName, string password)
         {
-            return true;
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Anusha\Documents\Projects\DB_PresentationLayer\App_Data\Database1.mdf;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select count(*) from Users where UserName='"+userName +"' and Password='"+password+"'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows[0][0].ToString() == "1")
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
