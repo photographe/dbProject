4976b008503482f59503c09e1c865259a9bd55c0
adudi1 <adudi1@umbc.edu>
2017-12-01 22:36:54 -0500

    added files and function definitions

---
+++ DB_PresentationLayer/BusinessLayer/BookingHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DB_PresentationLayer.EntityClass;
+
+namespace DB_PresentationLayer.BusinessLayer
+{
+    public class BookingHandler
+    {
+        public Booking FindRoute(Booking obj, string username = "")
+        {
+            return obj;
+        }
+        public bool Book(Booking obj, string username)
+        {
+            return true;
+        }
+    }
+}
