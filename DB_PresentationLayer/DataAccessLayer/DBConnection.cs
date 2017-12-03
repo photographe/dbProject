a2b9f14ba5469e75f1d1fe7ac59877ada0be132e
photographe <agniv1@umbc.edu>
2017-12-03 11:38:58 -0500

    modified profile handler

--- DB_PresentationLayer/DataAccessLayer/DBConnection.cs
+++ DB_PresentationLayer/DataAccessLayer/DBConnection.cs
@@ -10,22 +10,8 @@ namespace DB_PresentationLayer.DataAccessLayer
     public sealed class DBConnection
     {
         private static readonly DBConnection instance = new DBConnection();
-<<<<<<< HEAD
-        //private readonly SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Anusha\Documents\Projects\DB_PresentationLayer\App_Data\Database1.mdf;Integrated Security=True");
-        private readonly SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\baner\source\repos\DB_PresentationLayer\DB_PresentationLayer\App_Data\Database1.mdf;Integrated Security=True");
-
-=======
+        //private readonly SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\baner\source\repos\DB_PresentationLayer\DB_PresentationLayer\App_Data\Database1.mdf;Integrated Security=True");
         private readonly SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Anusha\Documents\Projects\DB_PresentationLayer\App_Data\Database1.mdf;Integrated Security=True");
-
-        static DBConnection()
-        {
-
-        }
-        private DBConnection()
-        {
-
-        }
->>>>>>> 21032bfb1440ddb4ecfe232d1a0e4cec5e3311de
         public static DBConnection Instance
         {
             get
